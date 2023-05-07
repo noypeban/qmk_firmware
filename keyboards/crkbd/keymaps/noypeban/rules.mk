995f7469fda39b93a07c2d5fd63a74a0c8f96388
noyp.eban <noyp.eban@gmail.com>
2023-05-07 13:39:07 +0900

    add my files

---
+++ keyboards/crkbd/keymaps/noypeban/rules.mk
@@ -0,0 +1,7 @@
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+AUTO_SHIFT_ENABLE = yes
+# VIA_ENABLE = yes
